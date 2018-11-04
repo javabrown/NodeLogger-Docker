ce209e44d79e4ab6585aba994541c1cff67a0500
Raja Khan <rkhan@raileurope.com>
2018-11-04 16:33:28 -0500

    To log everything coming in request body

--- build-docker-image.bat
+++ build-docker-image.bat
@@ -1,1 +1,3 @@
+docker login
 docker build -t getrk/node-web-app .
+docker push getrk/node-web-app
--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 'use strict';
 const express = require('express');
+const auth = require('basic-auth');
+const bodyParser = require('body-parser');
 
-// Constant
 const PORT = 7080;
 const HOST = '0.0.0.0';
+const NO_CONTENT_HTTP_204 = 204;
 
-//App
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-   res.send('hello world\n');
+   res.send(Date.now() + ' : hello :) from system monitor\n');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+/**
+ * Returns HTTP 204 and print request on console.
+ */
+app.post('/monitor', (request, response) => {
+   response.status(NO_CONTENT_HTTP_204).send();
+   const credentials = auth(request);
+   
+   if (credentials && credentials.name) {
+	  request.body.user = credentials.name;   
+   }
+    
+    console.log(JSON.stringify(request.body));
+});
+
+app.get('/monitor', (request, response) => {
+	response.send();
+});
+
+app.listen(PORT, function(){
+  console.log(`Running monitor on on http://${HOST}:${PORT}`);	
+});
